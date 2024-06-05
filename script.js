95262e577819cacd4b44fba7b19978d446fa544d
ItsYaBalay <yassin1999@gmail.com>
2024-06-05 15:37:42 -0400

    almost complete

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ const freelancers = [
   { name: "Prof. Goose", price: 72, occupation: "driver" },
 ];
 
+const init = () => {
+  const start = document.querySelector("body");
+  start.style.backgroundColor = "lightblue";
+  start.style.height = "300px";
+  start.style.width = "100%";
+  addOl(start);
+  addElement("h1", start, `Average price: $${averagePrice()}`);
+};
+
 const addElement = (element, parent, str) => {
   const text = document.createElement(element);
   text.textContent = str;
@@ -16,50 +25,40 @@ const addElement = (element, parent, str) => {
 };
 
 const addLi = (ol) => {
-    freelancers.forEach((str) => {
-        addElement(
-          "li",
-          ol,
-          `${str.name} with a price $${str.price} and occupation ${str.occupation}`
-        );
-      });
-}
+  freelancers.forEach((str) => {
+    addElement(
+      "li",
+      ol,
+      `${str.name} with a price $${str.price} and occupation ${str.occupation}`
+    );
+  });
+};
 
 const addOl = (parent) => {
   const orderList = document.createElement("ol");
-  addLi(orderList)
+  addLi(orderList);
+  addFreelancer(orderList);
   parent.append(orderList);
 };
 
 const averagePrice = () => {
   return (
-    freelancers.reduce((acc, curr) => acc + curr.price) / freelancers.length
+    Math.round(freelancers.reduce((acc, curr) => acc + curr.price, 0) / freelancers.length * 100)/ 100
   );
 };
 
 const addFreelancer = (parent) => {
-    const freelancer = {
-      name: "New freelancer",
-      price: Math.floor(Math.random() * 100),
-      occupation: "New",
-    };
-    addElement("li", parent, `${freelancer.name} with a price $${freelancer.price} and occupation ${freelancer.occupation}`)
-  }
-
-const init = () => {
-  const start = document.querySelector("body");
-  start.style.backgroundColor = "lightblue";
-  start.style.height = "300px";
-  start.style.width = "100%";
-  addElement("h1", start, `${averagePrice()}`);
-  setInterval(addFreelancer(start), 2000);
-  addOl(start);
+  const freelancer = {
+    name: "New freelancer",
+    price: Math.floor(Math.random() * 100),
+    occupation: "New",
+  };
+  addElement(
+    "li",
+    parent,
+    `${freelancer.name} with a price $${freelancer.price} and occupation ${freelancer.occupation}`
+  );
+  freelancers.push(freelancer)
 };
 
-
 init();
-
-
-
-
-
